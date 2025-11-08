39e3f2f612acc3eba573c1f8c9ca9658ca55326d
isaque1717-célula <ia257355@gmail.com>
2025-11-07 21:51:17 -0300

    conversor de brl para usd fucionando

--- scripts.js
+++ scripts.js
@@ -2,11 +2,22 @@ const convertButton = document.querySelector(".convetButton")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".inputCurrency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor real
+    const currencyValueToConverted = document.querySelector(".currency-value") // Outras moedas
 
     const dolarToday = 5.2
 
     const convertedValue = inputCurrencyValue / dolarToday
 
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
+        style: "currency", 
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+       style: "currency",
+       currency: "USD" 
+    }).format(convertedValue)
 
     console.log(convertedValue)
 }
