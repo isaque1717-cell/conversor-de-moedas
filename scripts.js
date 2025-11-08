c3b458ef03add0eadbe759c9286bed6a209de664
isaque1717-célula <ia257355@gmail.com>
2025-11-07 23:16:22 -0300

    conversor de moedas pc version pronto

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-const convertButton = document.querySelector(".convetButton")
+const convertButton = document.querySelector(".convertButton")
 const currencySelect = document.querySelector(".currency-select")
+
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".inputCurrency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor real
@@ -8,6 +9,7 @@ function convertValues() {
 
     const dolarToday = 5.2
     const euroToDay = 6.2
+    const bitcoinToDay =  10.5
 
 
     if (currencySelect.value == "dolar") {
@@ -26,6 +28,14 @@ function convertValues() {
 
     }
 
+    if (currencySelect.value == "bitcoin") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrencyValue / bitcoinToDay)
+
+    }
+
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
@@ -33,5 +43,34 @@ function convertValues() {
     }).format(inputCurrencyValue)
 
 }
+
+function changeCurrency (){
+   const currencyName = document.getElementById("currency-name")
+   const currencyImg = document.querySelector(".currency-img")
+
+   if (currencySelect.value == "dolar") {
+
+    currencyName.innerHTML = "dolar americano"
+    currencyImg.src = "assets/estados-unidos (1) 1.png"
+
+   }
+
+      if (currencySelect.value == "euro") {
+
+    currencyName.innerHTML = "Euro"
+    currencyImg.src = "assets/euro.png"
+
+   }
+      if (currencySelect.value == "bitcoin") {
+
+    currencyName.innerHTML = "bitcoin"
+    currencyImg.src = "assets/bitcoin.png"
+
+   }
+  
+   convertValues()
+}
+
+currencySelect.addEventListener("change",changeCurrency )
 
 convertButton.addEventListener("click", convertValues)
